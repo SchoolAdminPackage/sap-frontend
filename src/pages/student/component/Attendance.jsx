4369727bef7c59fa7bad5037187a2e18fc54e36b
Jetmate <antonoutkine@gmail.com>
2017-09-22 23:44:58 -0500

    Add basic student page

---
+++ src/pages/student/component/Attendance.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// import style from './stylesheet.css'
+
+class Component extends React.Component {
+  render () {
+    return (
+      <p>attendance</p>
+    )
+  }
+}
+
+export default Component
+
--- src/pages/student/index.jsx
+++ src/pages/student/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 
+import style from './stylesheet.css'
+import Attendance from './component/Attendance.jsx'
+import Grades from './component/Grades.jsx'
+
 class Main extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      layout: 'attendance'
+    }
+  }
   render () {
     return (
       <div>
-        test
+        <div className={`row-2 ${style.menuBar}`}>
+          <div className='button' onClick={() => this.setState({layout: 'attendance'})}>attendance</div>
+          <div className='button' onClick={() => this.setState({layout: 'grades'})}>grades</div>
+        </div>
+
+        {
+          this.state.layout === 'attendance' ? (
+            <Attendance />
+          ) : (
+            <Grades />
+          )
+        }
       </div>
     )
   }
