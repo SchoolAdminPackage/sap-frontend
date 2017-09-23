44c1230ba2d485b822778c04ecd0a63c6c19e99b
Christopher Fischer <christopherfischer@Christophers-MacBook-Pro.local>
2017-09-23 04:35:44 -0500

    Add average function

--- src/pages/student/component/Attendance.jsx
+++ src/pages/student/component/Attendance.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
 import style from '../stylesheet.css'
+<<<<<<< Updated upstream
 
+=======
+import { isoDate } from '../../../common/helpers.js'
+import { graphql, gql } from 'react-apollo'
+
+// import casual from 'casual'
+
+// import style from './stylesheet.css'
+// let query = gql`
+// query myQuery($student: String) {
+//   attendance(student: $student) {
+//     type
+//     date
+//   }
+// }
+// `
+
+// @graphql(query, {
+//   options: {
+//     variables: { student: window.localStorage.student }
+//   }
+// })
+>>>>>>> Stashed changes
 
 class Component extends React.Component {
   constructor (props) {
@@ -10,20 +33,32 @@ class Component extends React.Component {
     ///// DEBUG ONLY
     this.prop = {}
     this.prop.data = {}
+<<<<<<< Updated upstream
     this.prop.data.attendance = []
     for (let i = 0; i < 100; i++) {
       this.prop.data.attendance.push({date: i, type: true})
     }
+=======
+    this.prop.data.attendance = [{date: '03', type: true}, {date: '04', type: false}, {date: '06', type: true}, {date: '07', type: true}]
+>>>>>>> Stashed changes
     //////
   }
 
 
   render () {
     return (
+<<<<<<< Updated upstream
       <ul className={`${style.attendanceWrapper}`}>
         {this.prop.data.attendance.map((attendance) => (
           <li key={attendance.date} ><p style={{backgroundColor: (attendance.type ? 'red' : 'yellow')}}>{attendance.date}</p></li>
         ))}
+=======
+      <div className={`${style.wrapper}`}>
+      <ul className={`${style.columns}`}>
+      {this.prop.data.attendance.map((attendance) => (
+        <li className={`${style.attenBox}`} key={attendance.date} ><p style={{backgroundColor: (attendance.type ? 'red' : 'yellow'), width: '50px',padding:'20px', top:'0px'}}>{attendance.date}</p></li>
+      ))}
+>>>>>>> Stashed changes
       </ul>
     )
   }
--- src/pages/student/component/Grades.jsx
+++ src/pages/student/component/Grades.jsx
@@ -32,23 +32,51 @@ class Component extends React.Component {
     ///// DEBUG ONLY
     this.prop = {}
     this.prop.data = {}
-    this.prop.data.grades = [{course: 'math', name: 'test', percent: 20}, {course: 'math', name: 'test2', percent: 20}, {course: 'english', percent: 59, name: 'quiz'}, {course: 'cs', percent: 99, name: 'project'}]
+    this.prop.data.assignments = [{course: 'math', name: 'test', percent: 20}, {course: 'math', name: 'test2', percent: 30}, {course: 'english', percent: 59, name: 'quiz'}, {course: 'cs', percent: 99, name: 'project'}]
     this.prop.data.courses = [{name: 'math'}, {name: 'english'}, {name: 'cs'}]
     //////
   }
+  
+  
 
   render () {
+  	function calcGrade(assignments, course) {
+                      		let x = 0
+                      		let amt = 0
+                      		for(let i = 0; i<assignments.length; i++) {
+                      			if(assignments[i].course == course.name) {
+                      				x += assignments[i].percent
+                      				amt++
+                      			}
+                      		}
+
+                      		x = x/amt
+                      		return x
+                      	}
+                      	
     return (
       <ul className={style.grades}>
         {this.prop.data.courses.map((course) => (
             <li key={course.name} className={style.course}>
               <p>{course.name}</p>
               <ul>
-                {this.prop.data.grades.map((grade) => (
-                  grade.course === course.name ? (
-                    <li key={grade.name}>
-                      <p>{grade.name}</p>
-                      <p>{grade.percent}</p>
+                {this.prop.data.assignments.map((assignment) => (
+                  assignment.course === course.name ? (
+                    <li key={assignment.name}>
+                      <p>{assignment.name}</p>
+                      <p>here -> {
+
+                      	calcGrade(this.prop.data.assignments,course)
+
+                      	 
+                      	
+
+                      	
+                      	
+                      }
+
+
+                       here</p>
                     </li>
                   ) : ''
                 ))}
