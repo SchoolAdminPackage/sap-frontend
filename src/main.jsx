f896e8cbbfe48c44c82d128b0acd92205c95d9f7
Jetmate <antonoutkine@gmail.com>
2017-09-22 23:29:55 -0500

    Add basic page structure

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 // import { ApolloClient, ApolloProvider } from 'react-apollo'
 
-import Main from './pages/main/index.jsx'
+import Student from './pages/student/index.jsx'
+import Teacher from './pages/teacher/index.jsx'
+import Admin from './pages/admin/index.jsx'
+import Invalid from './pages/invalid/index.jsx'
 
 // const client = new ApolloClient()
 
+///////// DEBUG ONLY
+window.localStorage.layout = 'student'
+/////////
+
+
+let Component
+switch (window.localStorage.layout) {
+  case 'student':
+    Component = Student
+    break
+  case 'teacher':
+    Component = Teacher
+    break
+  case 'admin':
+    Component = Admin
+    break
+  case 'invalid':
+    Component = Invalid
+    break
+}
+
+
 ReactDOM.render(
   // <ApolloProvider client={client}>
-    <Main />
+    <Component />
   // </ApolloProvider>
   ,
   document.getElementById('app')
---
+++ src/pages/student/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Main extends React.Component {
+  render () {
+    return (
+      <div>
+        test
+      </div>
+    )
+  }
+}
+
+export default Main
