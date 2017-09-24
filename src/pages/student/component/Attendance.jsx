74cdb2de18ae8a8d605d408db833e4b57d54da87
Jetmate <antonoutkine@gmail.com>
2017-09-24 01:18:38 -0500

    Fix merge conflict

--- src/pages/student/component/Attendance.jsx
+++ src/pages/student/component/Attendance.jsx
@@ -17,10 +17,6 @@ export default class Component extends React.Component {
     })
       .then((response) => response.json())
       .then((attendance) => {
-<<<<<<< Updated upstream
-=======
-        console.log(attendance)
->>>>>>> Stashed changes
         this.setState({attendance: attendance.attendanceEvents})
       })
       this.state = {
@@ -33,11 +29,7 @@ export default class Component extends React.Component {
     return (
       <ul className={'student__attendance'}>
         {this.state.attendance.map((attendance) => (
-<<<<<<< Updated upstream
-          <li key={attendance.date}><p style={{backgroundColor: (attendance.tardy ? '#f1c40f' : '#b5525f')}} className='student__attendance_day'>{isoDate(attendance.date)}</p></li>
-=======
           <li key={attendance.date}><p style={{backgroundColor: (attendance.tardy ? '#b5525f' : '#f1c40f')}} className='student__attendance_day'>{attendance.date}</p></li>
->>>>>>> Stashed changes
         ))}
       </ul>
     )
